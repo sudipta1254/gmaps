99cd89ea098b8f5e811e830596e1fcbcdb38db00
Sudipta Singha <singhaashoka03@gmail.com>
2023-11-09 18:46:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var btn = $('button'), inp = $('input')
+var inp = $('input')
 
 $('button').click(function() {
    $('iframe').attr('src', `https://maps.google.com/maps?width=600&height=400&hl=en&q=${inp.val().trim()}&t=&z=11&ie=UTF8&iwloc=B&output=embed`)
