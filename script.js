70a4a0fc0b15c29b0e3f93a773072e3ec0032f0e
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-11-09 12:53:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var btn = $('button'), inp = $('input')
+
+$('button').click(function() {
+   $('iframe').attr('src', `https://maps.google.com/maps?width=600&height=400&hl=en&q=${inp.val().trim()}&t=&z=11&ie=UTF8&iwloc=B&output=embed`)
+})
